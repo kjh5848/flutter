dd8e50f5065714db83ae8f9733c97c8c5dc23521
kjh5848 <kjh5848@gmail.com>
2024-04-20 19:49:16 +0900

    프로필 아바타 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe01/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +11,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme(),
+      home: ProfilePage(),
+    );
+  }
+}
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body :Column(
+        children: [
+            Row(
+              children: [
+
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/avatar.png"),
+                  ),
+                )
+              ],
+            )
+
+        ],
+      )
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      leading: Icon(Icons.arrow_back_ios_new_rounded),
+      title: const Text('Profile'),
+      centerTitle: true,
+      elevation: 1.0,
+    );
   }
 }
